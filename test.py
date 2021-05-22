3a158d0455ac3f0ea123bed808c92b95b18309e4
biplob005 <84556230+biplob005@users.noreply.github.com>
2021-05-22 20:50:29 +0530

    Update test.py

--- test.py
+++ test.py
@@ -11,12 +11,7 @@ webrtc_ctx = webrtc_streamer(
     key="audio-filter",
     mode=WebRtcMode.SENDRECV,
     client_settings=WEBRTC_CLIENT_SETTINGS,
-
-    async_processing=True,
 )
 
-st.title("This is title")
-st.write("This is body")
-st.image("https://ggsc.s3.amazonaws.com/images/uploads/The_Science-Backed_Benefits_of_Being_a_Dog_Owner.jpg")
-st.video("https://youtu.be/awyNVPAH_o4")
+
 
