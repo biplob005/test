8ce1da25d26d8d55f1e12b7a2e679aa366551d6c
biplob005 <84556230+biplob005@users.noreply.github.com>
2021-05-22 20:50:04 +0530

    Update test.py

--- test.py
+++ test.py
@@ -1,7 +1,20 @@
 import streamlit as st
 from streamlit_webrtc import webrtc_streamer
 
-video = webrtc_streamer(key="example")
+WEBRTC_CLIENT_SETTINGS = ClientSettings(
+    rtc_configuration={"iceServers": [{"urls": ["stun:stun.l.google.com:19302"]}]},
+    media_stream_constraints={"video": True, "audio": True},
+)
+
+
+webrtc_ctx = webrtc_streamer(
+    key="audio-filter",
+    mode=WebRtcMode.SENDRECV,
+    client_settings=WEBRTC_CLIENT_SETTINGS,
+
+    async_processing=True,
+)
+
 st.title("This is title")
 st.write("This is body")
 st.image("https://ggsc.s3.amazonaws.com/images/uploads/The_Science-Backed_Benefits_of_Being_a_Dog_Owner.jpg")
