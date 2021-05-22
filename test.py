32a7a30add119efc05c68cbb8bc1dc5839e18eba
biplob005 <84556230+biplob005@users.noreply.github.com>
2021-05-22 20:31:11 +0530

    Update test.py

--- test.py
+++ test.py
@@ -1,4 +1,6 @@
 import streamlit as st
 from streamlit_webrtc import webrtc_streamer
 
-webrtc_streamer(key="example")
+video = webrtc_streamer(key="example")
+st.title("This is title")
+st.write("This is body")
