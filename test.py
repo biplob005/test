feb60df641af830546453c5d3fd4a665c2c3b717
biplob005 <84556230+biplob005@users.noreply.github.com>
2021-05-22 20:52:17 +0530

    Update test.py

--- test.py
+++ test.py
@@ -1,10 +1,6 @@
 import streamlit as st
 from streamlit_webrtc import webrtc_streamer
 
-WEBRTC_CLIENT_SETTINGS = ClientSettings(
-    rtc_configuration={"iceServers": [{"urls": ["stun:stun.l.google.com:19302"]}]},
-    media_stream_constraints={"video": True, "audio": True},
-)
 
 
 webrtc_ctx = webrtc_streamer(
