1d509323a13288d925ba56a6ff6fef4c41dfb4de
biplob005 <84556230+biplob005@users.noreply.github.com>
2021-05-22 20:33:55 +0530

    Update test.py

--- test.py
+++ test.py
@@ -4,3 +4,4 @@ from streamlit_webrtc import webrtc_streamer
 video = webrtc_streamer(key="example")
 st.title("This is title")
 st.write("This is body")
+st.image("https://ggsc.s3.amazonaws.com/images/uploads/The_Science-Backed_Benefits_of_Being_a_Dog_Owner.jpg")
