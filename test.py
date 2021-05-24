83134981e5042897f6d4ec2faf3d183eec38195a
biplob005 <84556230+biplob005@users.noreply.github.com>
2021-05-24 18:02:36 +0530

    Update test.py

--- test.py
+++ test.py
@@ -3,7 +3,7 @@ import streamlit as st
 
 from streamlit_webrtc import webrtc_streamer
 st.title('hello world')
-webrtc_streamer(key="example")
-
+video = webrtc_streamer(key="example")
+print(type(video))
 
 
